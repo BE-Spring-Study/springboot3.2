0d54596ae47504238f72d91b02a791500ab5dadd
tejava7177 <wngmstla@sch.ac.kr>
2023-09-26 22:50:11 +0900

    layout 템플릿 실습
    - 레이아웃 적용
    - p.85) 템플릿화 하여 html 코드를 변수화

--- src/main/java/org/studyalone/controller/FirstController.java
+++ src/main/java/org/studyalone/controller/FirstController.java
@@ -7,20 +7,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
 
-    @GetMapping("/hi")
-    public String niceToMeetYou(){
+    //greetings.mustache 파일 형식과 맞지 않으므로 오류
+//    @GetMapping("/hi")
+//    public String niceToMeetYou(){
+//        return "greetings";
+//    }
+
+    @GetMapping("/hi_juheun")
+    public String niceToMeetYou(Model model) {
+        //addAttribute("변수명", "변숫값")
+        model.addAttribute("username", "juheun");
         return "greetings";
     }
 
-//    @GetMapping("/hi")
-//    public String niceToMeetYou(Model model) {
-//        model.addAttribute("username", "hongpark");
-//        return "greetings";
-//    }
-//
-//    @GetMapping("/bye")
-//    public String seeYouNext(Model model) {
-//        model.addAttribute("nickname", "홍길동");
-//        return "goodbye";
-//    }
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("username", "주흔씨");
+        return "goodbye";
+    }
+
+    @GetMapping("/layout")
+    public String niceToMeetYou_layout(Model model) {
+        //addAttribute("변수명", "변숫값")
+        model.addAttribute("username", "juheun");
+        return "layoutstudy";
+    }
 }
